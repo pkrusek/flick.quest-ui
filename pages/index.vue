620af757d016a71cf2ef868c19be9b080031d26a
Pavel Krusek <pavel.krusek@gmail.com>
2022-05-12 15:01:15 +0200

    Test - environment.

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,31 @@
 <template>
-  <Tutorial />
+  <div>
+    <h1>Test - environment</h1>
+    <div v-for="item in items" :key="item.name">
+      {{item.name}}
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import axios from 'axios'
 
 export default Vue.extend({
   name: 'IndexPage',
+  data() {
+    return {
+      items: [],
+    }
+  },
+  created() {
+    axios.get('http://localhost:3001/items')
+      .then(response => {
+        this.items = response.data
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
 })
 </script>
